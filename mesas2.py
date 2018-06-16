9fcdf27de29d92ea83ee77426833ac06eacfa509
Dalex98 <39716896+Dalex98@users.noreply.github.com>
2018-06-16 11:19:02 -0500

    ya gente esta version, mesas2, solo tiene como input la cantidad de votantes que quieres. tiene 7 mesas, 6 normales y una preferncial. con el input de cantidad de votantes genera una lista con edades random. despues va sacando persona por persona y los manda a una mesa random. la politica para los discapacitados es que solo se acercan a la entrada y los mandan defrente a la mesa preferencial. esta hecho para que uno de cada 8 votantes sea discapacitado pero yo creo que enverdad eso depende de la cantidad de votantes. para probar con numero chicos funciona con ese parametro. leanlo y entiendanlo porque parte de la nota es explicar el trabajo.

---
+++ mesas2.py
@@ -0,0 +1,32 @@
+import random
+def edades(n):
+    edades = []
+    for i in range(n):
+        x = random.randint(18, 90)
+        edades.append(x)
+    return edades
+
+def hormiguero(lista):
+    mesas = {'Mesa n°1':[ ],'Mesa n°2':[ ],'Mesa n°3':[ ],'Mesa n°4':[ ],'Mesa n°5':[],'Mesa n°6':[],'Mesa Preferencial':[]}
+    counter=len(lista)//8
+    while counter>0:
+        manco=random.randint(0,len(lista))
+        key = 'Mesa Preferencial'
+        mesas.setdefault(key, [])
+        mesas[key].append(lista[manco])
+        counter=counter-1
+        del lista[manco]
+
+    for i in lista:
+        x=random.randint(1,6)
+        key='Mesa n°'+str(x)
+        mesas.setdefault(key,[ ])
+        mesas[key].append(i)
+    return mesas
+
+n=int(input("Numero de votantes:"))
+
+edades=edades(n)
+
+print (edades)
+print(hormiguero(edades))
