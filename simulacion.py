1982cd67f90d0e8884206222fea6f0469d12fe18
Dalex98 <39716896+Dalex98@users.noreply.github.com>
2018-06-20 17:02:24 -0500

    Escuchen whaa!

--- simulacion.py
+++ simulacion.py
@@ -1,11 +1,13 @@
 import mesas2
 import candidatos
 import random
-
+lista=[]
 edades=mesas2.edades
 votantes=mesas2.hormiguero(edades)
 postulantes=candidatos.candidatos
 
+print(votantes)
+print(postulantes)
 
 def escogervoto(votantes):
     counter=len(votantes)-1
@@ -13,17 +15,30 @@ def escogervoto(votantes):
         for i in votantes:
             key='Mesa n°'+str(counter)
             votan=votantes[key]
-        print(conteovoto(votan,postulantes))
+        print(voto(votan,postulantes))
         counter=counter-1
     keypref = 'Mesa Preferencial'
     votanpref = votantes[keypref]
     print(votanpref)
 
-def conteovoto(votan, postulantes):
+def voto(votan, postulantes):
     votos=[]
     for i in votan:
         votos.append(random.randint(1,len(postulantes)))
-    return votos
+    return conteovotos(votos,postulantes)
+
+def conteovotos(votos,postulantes):
+    for i in votos:
+        voto=postulantes[i+1]
+        registrovotos(voto,postulantes)
+
+def registrovotos(voto,postulantes):
+    for i in range(len(postulantes)):
+        lista.append(postulantes[0])
+        lista.append(voto)
+
+
 escogervoto(votantes)
 print(votantes)
 print(postulantes)
+print(lista)
